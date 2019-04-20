33735788fde902a996aa06ea9b8b1cc3b6dfb344
unknown <seylovie@gmail.com>
2019-04-20 15:00:23 -0700

    added a small comment for script

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 const link = document.getElementsByClassName("navigation__link");
 const checkbox = document.getElementById("nav-toggle");
 
+//added an event listener on all navigation links. Once clicked, closes the nav menu.
 for(let i = 0; i < link.length; i++){
     link[i].addEventListener('click', e => {
         checkbox.checked = false;
